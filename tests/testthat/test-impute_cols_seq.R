d0b1a2f69ddf4d93230c9e9da301065f8e27d558
torockel <rockel.to@gmail.com>
2022-01-24 08:35:17 +0100

    Improve style

--- tests/testthat/test-impute_cols_seq.R
+++ tests/testthat/test-impute_cols_seq.R
@@ -11,7 +11,6 @@ test_that("no data frame or no colnames throws an error", {
   )
 })
 
-
 test_that("complete columns and rows works", {
   ds_imp_test <- df_XYZ_10_mis
   M <- is.na(df_XYZ_10_mis)
@@ -26,10 +25,10 @@ test_that("complete columns and rows works", {
     impute_cols_seq(
       df_XYZ_10_mis,
       cols_used_for_imputation = "only_complete",
-      rows_used_for_imputation = "only_complete")
+      rows_used_for_imputation = "only_complete"
+    )
   )
 })
-
 
 test_that("complete columns and partly_complete rows work", {
   ds_imp_test <- df_XYZ_10_mis
@@ -44,7 +43,8 @@ test_that("complete columns and partly_complete rows work", {
     impute_cols_seq(
       df_XYZ_10_mis,
       cols_used_for_imputation = "only_complete",
-      rows_used_for_imputation = "partly_complete")
+      rows_used_for_imputation = "partly_complete"
+    )
   )
 })
 
@@ -53,14 +53,14 @@ test_that("complete columns and already_imputed rows work", {
   M <- is.na(df_XYZ_10_mis)
   ind_mis_X <- which(M[, "X"])
   ind_comp_X <- which(!M[, "X"])
-  for(ind_mis in seq_along(ind_mis_X)) {
-    lm_x <- lm(X ~ Z, ds_imp_test[c(ind_comp_X, ind_mis_X[seq_len(ind_mis -1 )]), ], na.action = na.fail)
+  for (ind_mis in seq_along(ind_mis_X)) {
+    lm_x <- lm(X ~ Z, ds_imp_test[c(ind_comp_X, ind_mis_X[seq_len(ind_mis - 1)]), ], na.action = na.fail)
     ds_imp_test$X[ind_mis_X[ind_mis]] <- predict(lm_x, ds_imp_test[ind_mis_X[ind_mis], ])
   }
   ind_mis_Y <- which(M[, "Y"])
   ind_comp_Y <- which(!M[, "Y"])
-  for(ind_mis in seq_along(ind_mis_Y)) {
-    lm_y <- lm(Y ~ Z, ds_imp_test[c(ind_comp_Y, ind_mis_Y[seq_len(ind_mis -1 )]), ], na.action = na.fail)
+  for (ind_mis in seq_along(ind_mis_Y)) {
+    lm_y <- lm(Y ~ Z, ds_imp_test[c(ind_comp_Y, ind_mis_Y[seq_len(ind_mis - 1)]), ], na.action = na.fail)
     ds_imp_test$Y[ind_mis_Y[ind_mis]] <- predict(lm_y, ds_imp_test[ind_mis_Y[ind_mis], ])
   }
 
@@ -69,7 +69,8 @@ test_that("complete columns and already_imputed rows work", {
     impute_cols_seq(
       df_XYZ_10_mis,
       cols_used_for_imputation = "only_complete",
-      rows_used_for_imputation = "already_imputed")
+      rows_used_for_imputation = "already_imputed"
+    )
   )
 })
 
@@ -77,12 +78,12 @@ test_that("complete columns and all_except_i_no_update rows work", {
   ds_imp_test <- df_XYZ_10_mis
   M <- is.na(df_XYZ_10_mis)
   ind_mis_X <- which(M[, "X"])
-  for(ind_mis in ind_mis_X) {
+  for (ind_mis in ind_mis_X) {
     lm_x <- lm(X ~ Z, df_XYZ_10[seq_len(nrow(df_XYZ_10))[-ind_mis], ], na.action = na.fail)
     ds_imp_test$X[ind_mis] <- predict(lm_x, df_XYZ_10[ind_mis, ])
   }
   ind_mis_Y <- which(M[, "Y"])
-  for(ind_mis in ind_mis_Y) {
+  for (ind_mis in ind_mis_Y) {
     lm_y <- lm(Y ~ Z, df_XYZ_10[seq_len(nrow(df_XYZ_10))[-ind_mis], ], na.action = na.fail)
     ds_imp_test$Y[ind_mis] <- predict(lm_y, df_XYZ_10[ind_mis, ])
   }
@@ -93,21 +94,21 @@ test_that("complete columns and all_except_i_no_update rows work", {
       df_XYZ_10, # use "completed" ds and M
       cols_used_for_imputation = "only_complete",
       rows_used_for_imputation = "all_except_i_no_update",
-      M = is.na(df_XYZ_10_mis))
+      M = is.na(df_XYZ_10_mis)
+    )
   )
 })
-
 
 test_that("complete columns and all_except_i rows work", {
   ds_imp_test <- df_XYZ_10
   M <- is.na(df_XYZ_10_mis)
   ind_mis_X <- which(M[, "X"])
-  for(ind_mis in ind_mis_X) {
+  for (ind_mis in ind_mis_X) {
     lm_x <- lm(X ~ Z, ds_imp_test[seq_len(nrow(ds_imp_test))[-ind_mis], ], na.action = na.fail)
     ds_imp_test$X[ind_mis] <- predict(lm_x, ds_imp_test[ind_mis, ])
   }
   ind_mis_Y <- which(M[, "Y"])
-  for(ind_mis in ind_mis_Y) {
+  for (ind_mis in ind_mis_Y) {
     lm_y <- lm(Y ~ Z, ds_imp_test[seq_len(nrow(ds_imp_test))[-ind_mis], ], na.action = na.fail)
     ds_imp_test$Y[ind_mis] <- predict(lm_y, ds_imp_test[ind_mis, ])
   }
@@ -118,7 +119,8 @@ test_that("complete columns and all_except_i rows work", {
       df_XYZ_10, # use "completed" ds and M
       cols_used_for_imputation = "only_complete",
       rows_used_for_imputation = "all_except_i",
-      M = is.na(df_XYZ_10_mis))
+      M = is.na(df_XYZ_10_mis)
+    )
   )
 })
 
@@ -126,12 +128,12 @@ test_that("complete columns and all rows work", {
   ds_imp_test <- df_XYZ_10
   M <- is.na(df_XYZ_10_mis)
   ind_mis_X <- which(M[, "X"])
-  for(ind_mis in ind_mis_X) {
+  for (ind_mis in ind_mis_X) {
     lm_x <- lm(X ~ Z, ds_imp_test, na.action = na.fail)
     ds_imp_test$X[ind_mis] <- predict(lm_x, ds_imp_test[ind_mis, ])
   }
   ind_mis_Y <- which(M[, "Y"])
-  for(ind_mis in ind_mis_Y) {
+  for (ind_mis in ind_mis_Y) {
     lm_y <- lm(Y ~ Z, ds_imp_test, na.action = na.fail)
     ds_imp_test$Y[ind_mis] <- predict(lm_y, ds_imp_test[ind_mis, ])
   }
@@ -142,7 +144,8 @@ test_that("complete columns and all rows work", {
       df_XYZ_10, # use "completed" ds and M
       cols_used_for_imputation = "only_complete",
       rows_used_for_imputation = "all",
-      M = is.na(df_XYZ_10_mis))
+      M = is.na(df_XYZ_10_mis)
+    )
   )
 })
 
@@ -150,12 +153,12 @@ test_that("complete columns and all_no_update rows work", {
   ds_imp_test <- df_XYZ_10_mis
   M <- is.na(df_XYZ_10_mis)
   ind_mis_X <- which(M[, "X"])
-  for(ind_mis in ind_mis_X) {
+  for (ind_mis in ind_mis_X) {
     lm_x <- lm(X ~ Z, df_XYZ_10, na.action = na.fail)
     ds_imp_test$X[ind_mis] <- predict(lm_x, df_XYZ_10[ind_mis, ])
   }
   ind_mis_Y <- which(M[, "Y"])
-  for(ind_mis in ind_mis_Y) {
+  for (ind_mis in ind_mis_Y) {
     lm_y <- lm(Y ~ Z, df_XYZ_10, na.action = na.fail)
     ds_imp_test$Y[ind_mis] <- predict(lm_y, df_XYZ_10[ind_mis, ])
   }
@@ -166,6 +169,7 @@ test_that("complete columns and all_no_update rows work", {
       df_XYZ_10, # use "completed" ds and M
       cols_used_for_imputation = "only_complete",
       rows_used_for_imputation = "all_no_update",
-      M = is.na(df_XYZ_10_mis))
+      M = is.na(df_XYZ_10_mis)
+    )
   )
 })
