031c1347699bc4539379ee1c6b45c98914fdcf41
torockel <rockel.to@gmail.com>
2022-01-25 17:06:15 +0100

    Add tests for max_iter

--- tests/testthat/test-impute_iterative.R
+++ tests/testthat/test-impute_iterative.R
@@ -7,3 +7,31 @@ test_that("initial_imputation_fun works", {
       )
   )
 })
+
+test_that("initial_imputation_fun and max_iter = 1 works", {
+  ds_imp <- missMethods::impute_mean(df_XYZ_10_mis)
+  ds_imp <- impute_cols_seq(ds_imp, M = is.na(df_XYZ_10_mis))
+  expect_equal(
+    ds_imp,
+    impute_iterative(
+      df_XYZ_10_mis,
+      initial_imputation_fun = missMethods::impute_mean, max_iter = 1
+    )
+  )
+})
+
+test_that("initial_imputation_fun and max_iter = 2 works", {
+  ds_imp <- missMethods::impute_mean(df_XYZ_10_mis)
+  for(i in 1:2) {
+    ds_imp <- impute_cols_seq(ds_imp, M = is.na(df_XYZ_10_mis), rows_used_for_imputation = "all_no_update")
+  }
+  expect_equal(
+    ds_imp,
+    impute_iterative(
+      df_XYZ_10_mis,
+      initial_imputation_fun = missMethods::impute_mean, max_iter = 2,
+      rows_used_for_imputation = "all_no_update"
+    )
+  )
+})
+
