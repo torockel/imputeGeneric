6e1904f78fa950791c6c0efc9457acc2b2961ca9
torockel <rockel.to@gmail.com>
2022-01-26 10:04:51 +0100

    Change defaults of rows/cols used for imputation

--- tests/testthat/test-impute_iterative.R
+++ tests/testthat/test-impute_iterative.R
@@ -15,11 +15,16 @@ test_that("Warning for incomplete ds is shown (only once)", {
 
 test_that("initial_imputation_fun and max_iter = 1 works", {
   ds_imp <- missMethods::impute_mean(df_XYZ_10_mis)
-  ds_imp <- impute_cols_seq(ds_imp, M = is.na(df_XYZ_10_mis))
+  ds_imp <- impute_cols_seq(
+    ds_imp, cols_used_for_imputation = "all_no_update",
+    rows_used_for_imputation = "all_no_update", M = is.na(df_XYZ_10_mis)
+    )
   expect_equal(
     ds_imp,
     impute_iterative(
       df_XYZ_10_mis,
+      cols_used_for_imputation = "all_no_update",
+      rows_used_for_imputation = "all_no_update",
       initial_imputation_fun = missMethods::impute_mean, max_iter = 1
     )
   )
@@ -28,13 +33,18 @@ test_that("initial_imputation_fun and max_iter = 1 works", {
 test_that("initial_imputation_fun and max_iter = 2 works", {
   ds_imp <- missMethods::impute_mean(df_XYZ_10_mis)
   for(i in 1:2) {
-    ds_imp <- impute_cols_seq(ds_imp, M = is.na(df_XYZ_10_mis), rows_used_for_imputation = "all_no_update")
+    ds_imp <- impute_cols_seq(
+      ds_imp, M = is.na(df_XYZ_10_mis),
+      cols_used_for_imputation = "all_no_update",
+      rows_used_for_imputation = "all_no_update"
+    )
   }
   expect_equal(
     ds_imp,
     impute_iterative(
       df_XYZ_10_mis,
       initial_imputation_fun = missMethods::impute_mean, max_iter = 2,
+      cols_used_for_imputation = "all_no_update",
       rows_used_for_imputation = "all_no_update"
     )
   )
