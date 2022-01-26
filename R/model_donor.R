b907e70cf607c9baf37ccdb2f2101659b30ffb19
torockel <rockel.to@gmail.com>
2022-01-26 20:12:56 +0100

    Add donor average

--- R/model_donor.R
+++ R/model_donor.R
@@ -3,10 +3,16 @@ model_donor <- function(ds, M, i, donor_selection = "simultan_complete") {
     suitable_rows <- complete.cases(ds)
   } else if(donor_selection == "simultan_incomplete") {
     suitable_rows <- apply(M, 1, function(x) !any(M[i, ] & x))
+  } else {
+    stop(paste0("'", donor_selection, "' is not a valid option for donor_selection"))
   }
   return(structure(ds[suitable_rows, ], donor_selection = donor_selection))
 }
 
-predict_donor <- function(model_hd, ds_used, M, i, donor_aggregation = "choose_random") {
-  model_hd[sample.int(nrow(model_hd), 1), M[i, ]]
+predict_donor <- function(ds_donors, ds_used, M, i, donor_aggregation = "choose_random") {
+  if (donor_aggregation == "choose_random") {
+    return(ds_donors[sample.int(nrow(ds_donors), 1), M[i, ]])
+  } else if (donor_aggregation == "average") {
+    return(colMeans(ds_donors[ , M[i, ], drop = FALSE]))
+  }
 }
--- tests/testthat/test-model_donor.R
+++ tests/testthat/test-model_donor.R
@@ -1,7 +1,20 @@
-test_that("donor simultan_complete works", {
+test_that("model_donor simultan_complete works", {
   hd_model <- model_donor(df_XYZ_10_mis, is.na(df_XYZ_10_mis), 2, donor_selection = "simultan_complete")
   expect_true(all(
     rep.int(predict_donor(hd_model, df_XYZ_10_mis, is.na(df_XYZ_10_mis), 2) %in%
             seq_len(10)[-c(2, 7)], 50)
   ))
+})
+
+test_that("predict_donor average works", {
+  donors_i_2 <- model_donor(df_XYZ_10_mis, is.na(df_XYZ_10_mis), 2, donor_selection = "simultan_complete")
+  expect_equal(
+    predict_donor(donors_i_2, df_XYZ_10_mis, M = is.na(df_XYZ_10_mis), i = 2, "average"),
+    colMeans(df_XYZ_10_mis[complete.cases(df_XYZ_10_mis), "X", drop = FALSE])
+  )
+  donors_i_7 <- model_donor(df_XYZ_10_mis, is.na(df_XYZ_10_mis), 7, donor_selection = "simultan_complete")
+  expect_equal(
+    predict_donor(donors_i_7, df_XYZ_10_mis, M = is.na(df_XYZ_10_mis), i = 7, "average"),
+    colMeans(df_XYZ_10_mis[complete.cases(df_XYZ_10_mis), c("X", "Y")])
+  )
 })
