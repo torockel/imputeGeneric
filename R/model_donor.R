66151c5ee81953e59fdc71682a097c26f9db460f
torockel <rockel.to@gmail.com>
2022-01-26 20:45:39 +0100

    Add knn_simultan

--- R/model_donor.R
+++ R/model_donor.R
@@ -1,10 +1,15 @@
-model_donor <- function(ds, M, i, donor_selection = "simultan_complete") {
-  if (donor_selection == "simultan_complete") {
+model_donor <- function(ds, M, i, donor_selection = "simultan_complete", donor_k = 10) {
+  if (donor_selection %in% c("simultan_complete", "knn_simultan_complete")) {
     suitable_rows <- complete.cases(ds)
-  } else if(donor_selection == "simultan_incomplete") {
+  } else if(donor_selection %in% c("simultan_incomplete", "knn_simultan_incomplete")) {
     suitable_rows <- apply(M, 1, function(x) !any(M[i, ] & x))
   } else {
     stop(paste0("'", donor_selection, "' is not a valid option for donor_selection"))
+  }
+  if (donor_selection %in% c("knn_simultan_complete", "knn_simultan_incomplete")) {
+    suitable_rows_ind <- which(suitable_rows)
+    best_k <- gower::gower_topn(ds[i, ], ds[suitable_rows, ], n = donor_k)$index[, 1]
+    suitable_rows <- suitable_rows_ind[best_k]
   }
   return(structure(ds[suitable_rows, ], donor_selection = donor_selection))
 }
--- tests/testthat/test-model_donor.R
+++ tests/testthat/test-model_donor.R
@@ -6,6 +6,17 @@ test_that("model_donor simultan_complete works", {
   ))
 })
 
+# Add tests for simultan_incomplete and knn_simultan_incomplete
+
+test_that("model_donor knn_simultan_complete works", {
+  comp_cases <- df_XYZ_10_mis[complete.cases(df_XYZ_10_mis), ]
+  top_3 <- gower::gower_topn(df_XYZ_10_mis[2, ], comp_cases, n = 3)$index[, 1]
+  expect_equal(
+    structure(comp_cases[top_3, ], donor_selection = "knn_simultan_complete"),
+    model_donor(df_XYZ_10_mis, is.na(df_XYZ_10_mis), 2, donor_selection = "knn_simultan_complete", donor_k = 3)
+  )
+})
+
 test_that("predict_donor average works", {
   donors_i_2 <- model_donor(df_XYZ_10_mis, is.na(df_XYZ_10_mis), 2, donor_selection = "simultan_complete")
   expect_equal(
