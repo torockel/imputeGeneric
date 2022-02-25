f9cf7885da47a0177c814c68cd01cf4225f98b3f
torockel <rockel.to@gmail.com>
2022-02-25 17:08:33 +0100

    Remove unused data set

--- tests/testthat/helper-datasets.R
+++ tests/testthat/helper-datasets.R
@@ -7,8 +7,6 @@ df_XYZ_10_mis <- df_XYZ_10
 df_XYZ_10_mis[c(2, 7), "X"] <- NA
 df_XYZ_10_mis[c(1, 4, 7), "Y"] <- NA
 
-df_XY_10_X_mis <- df_XYZ_10_mis[, c("X", "Y")]
-
 
 # info_list for testing stop funs ---------------------------------------------
 info_list <- list(M = is.na(df_XYZ_10_mis), nr_iterations = 42, max_iter = 70)
